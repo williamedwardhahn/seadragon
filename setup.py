326dccda0b3af0401a6a8394e1c352019503f136
William E Hahn <williamedwardhahn@gmail.com>
2020-10-19 20:00:59 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='seadragon',
+      version='0.1',
+      description='example',
+      url='https://github.com/williamedwardhahn/seadragon',
+      author='W Hahn',
+      author_email='williamedwardhahn@gmail.com',
+      license='MIT',
+      packages=['seadragon'],
+      zip_safe=False)
